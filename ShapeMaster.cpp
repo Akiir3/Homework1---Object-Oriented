6f82f742a19d4d89d34b3fd1420d540bd12b3eb9
Erika Bailon <36748447+Akiir3@users.noreply.github.com>
2019-02-01 00:37:57 -0700

    Create ShapeMaster.cpp

---
+++ ShapeMaster.cpp
@@ -0,0 +1,18 @@
+#include "ShapeMaster.h"
+#include "Triangle.h"
+#include "Square.h"
+#include "Circle.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+
+ShapeMaster::ShapeMaster(){
+	float area = 0;
+	
+}
+
+float ShapeMaster::theArea(){
+	return area;
+}
