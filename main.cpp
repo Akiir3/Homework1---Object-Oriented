569e1b0a8afaa7fd5e6eb541b4a0a7473683bfde
Erika Bailon <erba0445@colorado.edu>
2020-01-14 09:44:39 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,8 +12,6 @@
 
 using namespace std;
 
-
-
 int main(){
   
   //Here I am   constructing the shapes with given dimensions
@@ -24,15 +22,8 @@ int main(){
   Circle Shape5 (5.0, 7.5);
   Square Shape6 (2.0, 2.0);
 
-  // cout << Shape6.theArea() << endl;
-  // cout << typeid(Shape6).name() << endl;
-
   // initializing my vector with all the objects 
   vector<ShapeMaster> myVector; 
-
-  //initializing list 
-  // list<ShapeMaster> theList;
-
 
   //putting all the objects in the vector 
   myVector.push_back(Shape1);
@@ -42,31 +33,11 @@ int main(){
   myVector.push_back(Shape4);
   myVector.push_back(Shape6);
 
-  // theList.push_back(Shape1);
-  // theList.push_back(Shape2);
-  // theList.push_back(Shape3);
-  // theList.push_back(Shape4);
-  // theList.push_back(Shape5);
-  // theList.push_back(Shape6);
-
- //  theList.sort([](float ShapeMaster &f, float ShapeMaster &s) {
- //  	 return f.theArea < s.theArea; });
-
-	// for (auto it = theList.begin(); it != theList.end(); ++it) 
- //        cout << ' ' << *it;
-
   sort(myVector.begin(), myVector.end());
-
 
     for ( int i = 0; i < myVector.size(); ++i ) {
         cout << i + 1 << ". " << myVector[i].theArea() << "\t"<< typeid(myVector[i]).name() << endl;
         // cout<<typeid(Shape4).name()<<"\t"<< quote(Shape4) <<"\n";
         }
-  
 	return 0;
 }
-
-
-
-
-
